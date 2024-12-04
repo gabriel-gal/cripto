b2865c4fa27303aa4add09c5d8a8fb2fb7011799
Gabriel <pr911644@prodesan.combr>
2024-12-04 13:53:41 -0300

    refactory: ajustando o loader

--- src/components/CardCripto/index.tsx
+++ src/components/CardCripto/index.tsx
@@ -10,13 +10,26 @@ export default function CardCripto() {
     return (
         <div>
             <header className="flex items-center justify-between gap-3 text-white mb-2">
-                <div className="flex items-center gap-1 md:gap-3">
-                    <img className="w-10 h-10 md:w-20 md:h-20 rounded-full" src={data?.image} />
-                    <p className="text-xl lg:text-3xl">{data?.name}</p>
-                    <p className="text-lg lg:text-2xl">({data?.symbol})</p>
-                </div>
+                {data ? (
+                    <div className="flex items-center gap-1 md:gap-3">
+                        <img
+                            className="w-10 h-10 md:w-20 md:h-20 rounded-full"
+                            src={data?.image}
+                            alt={`Logo da Cripto ${data?.name}`}
+                        />
+                        <p className="text-xl lg:text-3xl">{data?.name}</p>
+                        <p className="text-lg lg:text-2xl">({data?.symbol})</p>
+
+                    </div>
+                ) : (
+                    <div className="flex items-center gap-1 md:gap-3">
+                        <div className="w-10 h-10 md:w-20 md:h-20 rounded-full bg-gray-200 animate-pulse"></div>
+                        <p className="text-xl lg:text-3xl text-gray-500 animate-pulse">Carregando...</p>
+                    </div>
+                )}
                 <div>
                     <p className="text-lg md:text-lg lg:text-2xl xl:text-3xl">
+
                         {data?.current_price.toLocaleString("pt-BR", {
                             style: "currency",
                             currency: "BRL",
